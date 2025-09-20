49b292eb067665a149ffc87b340201a635149f1c
MAmazigh <mohamedazi@yahoo.fr>
2025-09-20 17:17:24 +0200

    insert  enrich_iterator_with_sql_fragments_for_calculs

--- script/utils/sql_utils/__init__.py
+++ script/utils/sql_utils/__init__.py
@@ -3,3 +3,4 @@
 from .read_query import read_query
 from .enrich_iterator_with_sql_fragments_for_extraction import enrich_iterator_with_sql_fragments_for_extraction
 from .process_extraction_page_queries import process_extraction_page_queries
+from .enrich_iterator_with_sql_fragments_for_calculs import enrich_iterator_with_sql_fragments_for_calculs
---
+++ script/utils/sql_utils/enrich_iterator_with_sql_fragments_for_calculs.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+ Function sql_utils.enrich_iterator_with_sql_fragments_for_calculs: enrich SQL query
+"""
+
+import pandas as pd
+import numpy as np
+
+
+def enrich_iterator_with_sql_fragments_for_calculs(df: pd.DataFrame) -> pd.DataFrame:
+    """
+
+    :rtype: object
+    """
+    # We base our loops on a lookup table
+    # Ajout des indicateurs temporels (MC, MP, AP)
+    df_indic = pd.DataFrame({'key': 0, 'indic': ['MC', 'MP', 'AP']})
+    iterator = pd.merge(df, df_indic, on='key', how='outer')
+
+    # Suppression de MP si la période est Y ou P
+    mask = iterator.period.isin(['Y', 'P']) & (iterator.indic == 'MP')
+    iterator = iterator[~mask]
+
+    # Enrichissement des colonnes SQL dynamiques
+    iterator['freq'] = iterator['indic'].map(lambda x: f"freq_{x}")
+    iterator['threshold_NI_NS'] = self.threshold_NI_NS
+    iterator['indicateur'] = iterator['indic'].map(lambda x: f"indicateur_{x}")
+    iterator['fmt_evol'] = np.where(iterator.indic.isin(['MP', 'AP']), '', (
+        ", CASE WHEN evol_indicateur < -1.96 THEN '-' "
+        "WHEN evol_indicateur BETWEEN -1.96 AND 1.96 THEN '=' "
+        "WHEN evol_indicateur > 1.96 THEN '+' END as valeurs_e"
+    ))
+    iterator['kpi'] = np.where(iterator.indic.isin(['MP', 'AP']), ', kpi',
+                               ", CASE WHEN kpi in ('NI', 'NS') THEN kpi ELSE kpi||'|'||valeurs_e END as kpi")
+    iterator['flag_periode'] = iterator['indic'].map(lambda x: f", '{x}' AS periode")
+
+    return iterator
